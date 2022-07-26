01cb79243d535b65b332e4786d0db633de2a01f0
aurelie <aurelie.conan@lepetitvapoteur.com>
2022-07-26 14:45:59 +0200

    tuto angular

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[] = [];
+
+  selectedHero?: Hero;
+  
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+}
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+
+}
+
+
